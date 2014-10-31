dc179585cbc010efdf08cc50b95e9c9d0dd7bd84
Ricardo Heck <rheck@conrad-caine.com>
2014-10-31 13:06:16 -0200

    Fixed services.yml file name

--- DependencyInjection/RheckDatatableExtension.php
+++ DependencyInjection/RheckDatatableExtension.php
@@ -23,7 +23,7 @@ class RheckDatatableExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('services.yml');
 
         foreach ($config['service'] as $key => $service) {
             $container->setAlias($this->getAlias() . '.' . $key, $service);
